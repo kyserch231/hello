2c3338de1e3c3ae30c79473fb21e6c3df2c59f25
Christopher Kyser <kyserch@eos20.cis.gvsu.edu>
2021-05-27 15:12:59 -0400

    created some more files

---
+++ or.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+	double f = 0;
+	printf("enter a number: ");
+	scanf("%lf", &f);
+	if(f<100){
+		printf("you entered %g",f);
+	}
+	else{
+		printf("you entered %g",f);
+	}
+	return 0;	
+
+}
